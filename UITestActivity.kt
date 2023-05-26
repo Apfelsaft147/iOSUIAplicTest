5c87b8ba134d0f725f0b8443bad651d76ae87076
Apfelsaft147 <79417289+Apfelsaft147@users.noreply.github.com>
2023-05-26 20:16:03 +0200

    Add files via upload

---
+++ UITestActivity.kt
@@ -0,0 +1,12 @@
+package me.ibrahimsn.smoothbottombar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UITestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_uitest)
+        setTheme(R.style.AppTheme1)
+    }
+}
